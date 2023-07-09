68e768eeb4c34d41a49ccad9ec2e616b058fb1cc
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-09 23:40:14 +0530

    Completed

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Body({ imageData }) {
+  return (
+    <div>
+      {imageData.map(image => (
+        <div key={image.id} className="column">
+          <img src={image.img} alt=""></img>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Body;
